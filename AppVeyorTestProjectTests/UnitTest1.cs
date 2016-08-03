1127cd39edaacd4d0fdfff742e0d5adfdd4b250e
SStBC <spinspintheblackcircle@gmail.com>
2016-08-03 14:37:50 +0200

    Added unit tests.

---
+++ AppVeyorTestProjectTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AppVeyorTestProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppVeyorTestProjectTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var h = new Handler();
+            var i = 4;
+            Assert.AreEqual(i + 1, h.Increment(i));
+
+        }
+    }
+}
